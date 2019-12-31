b45284e99975d051a25e62392d49bdb79030acbc
EmperorBob7 <davinderpaltoor@gmail.com>
2019-12-30 21:40:57 -0600

    Color Update

--- JSheets/scripts.js
+++ JSheets/scripts.js
@@ -2,15 +2,16 @@ var rows = 0;
 var cols = 0;
 var letter = 65;
 var t = [];
-var selected = "";
+var selected = "a";
 class Cell {
 	constructor(id) {
 		this.id = id;
 		this.value = "";
-		this.color = "black";
-		this.backgroundColor = "gray";
+		this.color = "#000000";
+		this.backgroundColor = "#555555";
 	}
 }
+t["a"] = new Cell("a");
 window.onload = function() {
 	var table = document.getElementById("table");
 	for(var i = 1; i<=5; i++) {
@@ -34,11 +35,21 @@ window.onload = function() {
 function focusize(given) {
 	selected = given;
 	document.getElementById("typer").value = t[given].value;
+	document.getElementById("colorSelector").value = t[selected].color;
+	document.getElementById("bcSelector").value = t[selected].backgroundColor;
 	document.getElementById("typer").focus();
 }
 function submitVal() {
 	t[selected].value = document.getElementById("typer").value;
 	document.getElementById(selected).innerHTML = t[selected].value;
 	document.getElementById("typer").value = "";
-	selected = "";
+	selected = "a";
+}
+function cChange() {
+	t[selected].color = document.getElementById("colorSelector").value;
+	document.getElementById(selected).style.color = t[selected].color;
+}
+function bcChange() {
+	t[selected].backgroundColor = document.getElementById("bcSelector").value;
+	document.getElementById(selected).style.backgroundColor = t[selected].backgroundColor;
 }
