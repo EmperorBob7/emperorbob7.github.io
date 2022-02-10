9b6863640173b136892282410927543597819c7b
EmperorBob7 <davinderpaltoor@gmail.com>
2022-02-09 23:22:20 -0600

    Handle Guesses
    Created base 5 letter array
    Verified all words are valid
    Handled guesses
    Temporary color scheme
    Moving down rows
    Shaking animation
    Popup template box
    Class management on guesses
    Resetting guess
    
    TODO
    __________
    Randomizing word based on day
    Stopping game after 6 guesses
    Theoretically 5 letter and 6 letter versions
    Discord copy+paste

--- Triggle/scripts.js
+++ Triggle/scripts.js
@@ -1,12 +1,13 @@
-let table;
+let table, popUp;
 let term = [];
 let row = 1;
 
 window.onload = () => {
     table = document.getElementById("table");
-    for(let i = 0; i<6; i++) {
+    popUp = document.getElementById("popup");
+    for (let i = 0; i < 6; i++) {
         let tr = document.createElement("tr");
-        for(let j = 0; j< 6; j++) {
+        for (let j = 0; j < 5; j++) {
             let td = document.createElement("td");
             td.innerText = "\u00A0";
             tr.appendChild(td);
@@ -15,7 +16,7 @@ window.onload = () => {
     }
     let keyboardButtons = document.querySelectorAll("#keyboard > div > button");
     keyboardButtons.forEach(x => {
-        x.addEventListener("click", () => {type(x.innerText)});
+        x.addEventListener("click", () => { type(x.innerText) });
     });
 };
 
@@ -24,11 +25,58 @@ window.onload = () => {
  * @param {String} char The character typed or pressed
  */
 function type(char) {
-    console.log(char);
-    if(term.length < 6) {
+    if (Number.isNaN(char.charCodeAt(0))) {
+        if (term.length > 0) {
+            let elements = document.querySelectorAll(`tr:nth-child(${row}) > td`);
+            elements[term.length - 1].innerText = "\u00A0";
+            term.pop();
+        }
+        return;
+    }
+    if (char == "ENTER" && term.length != 5) {
+        shake();
+    } else if (char == "ENTER") {
+        handleGuess(term.join(""));
+    } else if (term.length != 5) {
         term.push(char);
         let elements = document.querySelectorAll(`tr:nth-child(${row}) > td`);
-        console.log(elements);
-        elements[term.length-1].innerText = term[term.length-1];
+        elements[term.length - 1].innerText = term[term.length - 1];
+    }
+    let guess = term.join("");
+    if (guess.length == 5 && !words.includes(guess)) {
+        shake();
     }
 }
+
+/**
+ * 
+ * @param {String} guess Their guess in all caps 
+ */
+function handleGuess(guess) {
+    console.log("guessed");
+    for (let i = 0; i < 5; i++) {
+        if (guess[i] == wordOfTheDay[i]) {
+            document.querySelector(`#table > tr:nth-child(${row}) > td:nth-child(${i + 1})`).classList.add("right");
+        } else if (wordOfTheDay.includes(guess[i])) {
+            document.querySelector(`#table > tr:nth-child(${row}) > td:nth-child(${i + 1})`).classList.add("half");
+        } else {
+            document.querySelector(`#table > tr:nth-child(${row}) > td:nth-child(${i + 1})`).classList.add("wrong");
+        }
+    }
+    term = [];
+    row++;
+}
+
+function shake() {
+    let r = document.querySelector(`#table > tr:nth-child(${row})`);
+    r.classList.add("shake");
+    setTimeout(() => {
+        r.classList.remove("shake");
+    }, 600);
+}
+
+const words = ["HOUND", "VIPER", "IZUMI", "YUIGA", "RINDO", "RAIZO", "KOHEI", "ISAMI", "SHIRO", "SAEKI", "KUROE", "SHUJI", "ASUMI", "KARIN", "REIJI", "KIRIE", "USAMI", "TSUJI", "IKOMA", "HOSOI", "MAORI", "AZUMA", "OSAMU", "CHIKA", "HYUSE", "MIURA", "SOMEI", "OSANO", "HIURA", "AKANE", "SHIKI", "TOMOE", "AYUMU", "EBINA", "CHANO", "SAITO", "NANAO", "RYOGO", "MARUI", "SEIJI", "ASAMI", "HANAO", "ASUKA", "KEIZO", "IZUHO", "HINOE", "TRION", "ILGAR", "NABIS", "SCARE", "RADAR", "RIFLE", "COBRA", "EGRET", "SENKU", "GEIST", "TIMER", "TOKEN"];
+
+const wordOfTheDay = words[15];
+
+console.log(wordOfTheDay);
