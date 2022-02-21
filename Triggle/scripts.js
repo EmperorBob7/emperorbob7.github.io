b4523364b05224b58ef2d59279f6c7a9f4bd9e1a
EmperorBob7 <davinderpaltoor@gmail.com>
2022-02-20 22:25:34 -0600

    Local Storage everything, all guesses + score distributions
    Cleared all console logs.
    Updated Table Styling.
    Made share button functional.
    
    TODO:
    Copy to clipboard

--- Triggle/scripts.js
+++ Triggle/scripts.js
@@ -1,6 +1,7 @@
 import * as random from "./random-seed.js";
-let table, popUp;
+let table, popUp, blocker;
 let term = [];
+let guessStorage = [];
 let row = 1;
 const seed = random.default;
 const generator = seed(new Date().toDateString() + "capybaras4life!");
@@ -8,8 +9,13 @@ let keys = {};
 let doneWithGame = false;
 
 window.onload = () => {
+    let data = JSON.parse(localStorage.getItem(new Date().toDateString()));
+
     table = document.getElementById("table");
     popUp = document.getElementById("popup");
+    blocker = document.getElementById("blocker");
+    blocker.addEventListener("click", closePopUp);
+
     for (let i = 0; i < 5; i++) {
         let tr = document.createElement("tr");
         for (let j = 0; j < 5; j++) {
@@ -19,6 +25,7 @@ window.onload = () => {
         }
         table.appendChild(tr);
     }
+
     let keyboardButtons = document.querySelectorAll("#keyboard > div > button");
     keyboardButtons.forEach(x => {
         x.addEventListener("click", () => { type(x.innerText) });
@@ -36,10 +43,26 @@ window.onload = () => {
 
     let keyButtons = document.querySelectorAll("div.row > button");
     keyButtons.forEach(x => {
-        if(x.classList.length == 0) {
+        if (x.classList.length == 0) {
             keys[x.innerText] = x;
         }
     });
+
+    document.getElementById("share").addEventListener("click", () => {
+        share();
+        popDisplay();
+    });
+
+    if (data != null) {
+        let tds = document.getElementsByTagName("td");
+        for (let i = 0; i < 25 && i < data.length * 5; i++) {
+            tds[i].innerText = data[Math.floor(i / 5)][i % 5];
+        }
+        data.forEach(x => {
+            term = x;
+            handleGuess(x.join(""));
+        });
+    }
 };
 
 /**
@@ -47,7 +70,7 @@ window.onload = () => {
  * @param {String} char The character typed or pressed
  */
 function type(char) {
-    if(doneWithGame)
+    if (doneWithGame)
         return;
     if (Number.isNaN(char.charCodeAt(0))) {
         if (term.length > 0) {
@@ -89,19 +112,23 @@ function handleGuess(guess) {
             keys[guess[i]].classList.add("wrong");
         }
     }
+    guessStorage.push(term);
+    localStorage.setItem(new Date().toDateString(), JSON.stringify(guessStorage));
     term = [];
     row++;
-    if(guess == wordOfTheDay || row > 5) {
+    if (guess == wordOfTheDay || row > 5) {
         doneWithGame = true;
+        updateData(guess);
+        share();
+        popDisplay();
     }
 }
 
 let shakeFlag = false;;
 
 function shake() {
-    if(shakeFlag)
+    if (shakeFlag)
         return;
-    console.log("sus");
     let r = document.querySelector(`#table > tr:nth-child(${row})`);
     r.style.transform = "translateX(0)";
     r.classList.add("shake");
@@ -112,8 +139,61 @@ function shake() {
     }, 600);
 }
 
+function updateData(guess) {
+    if (localStorage.getItem("played") === null) {
+        localStorage.setItem("played", 0);
+    }
+    if (localStorage.getItem("wins") === null) {
+        localStorage.setItem("wins", 0);
+    }
+    if (localStorage.getItem("streak") === null) {
+        localStorage.setItem("streak", 0);
+    }
+    if (localStorage.getItem("1") === null) {
+        for (let i = 1; i <= 6; i++) {
+            localStorage.setItem(i + "", 0);
+        }
+        localStorage.setItem("DNF", 0);
+    }
+    if (localStorage.getItem(new Date().toDateString() + "Completed") === null) {
+        if (row == 6) {
+            localStorage.setItem("DNF", Number(localStorage.getItem("DNF")) + 1);
+        } else {
+            localStorage.setItem(row - 1, Number(localStorage.getItem(row - 1)) + 1);
+        }
+        localStorage.setItem("played", Number(localStorage.getItem("played")) + 1);
+        if (guess == wordOfTheDay) {
+            localStorage.setItem("wins", Number(localStorage.getItem("wins")) + 1);
+            localStorage.setItem("streak", Number(localStorage.getItem("streak")) + 1);
+        } else {
+            localStorage.setItem("streak", 0);
+        }
+        localStorage.setItem(new Date().toDateString() + "Completed", true);
+    }
+}
+
+function share() {
+    document.getElementById("played").innerText = localStorage.getItem("played");
+    document.getElementById("win%").innerText = Math.round(Number(localStorage.getItem("wins")) / Number(localStorage.getItem("played")) * 100);
+    document.getElementById("streak").innerText = localStorage.getItem("streak");
+    if (localStorage.getItem("1")) {
+        for (let i = 1; i <= 6; i++) {
+            document.getElementById(i + "").innerText = localStorage.getItem(i + "");
+        }
+        document.getElementById("DNF").innerText = localStorage.getItem("DNF");
+    }
+}
+
+function popDisplay() {
+    popUp.style.display = "block";
+    blocker.style.display = "block";
+}
+
+function closePopUp() {
+    popUp.style.display = "none";
+    blocker.style.display = "none";
+}
+
 const words = ["HOUND", "VIPER", "IZUMI", "YUIGA", "RINDO", "RAIZO", "KOHEI", "ISAMI", "SHIRO", "SAEKI", "KUROE", "SHUJI", "ASUMI", "KARIN", "REIJI", "KIRIE", "USAMI", "TSUJI", "IKOMA", "HOSOI", "MAORI", "AZUMA", "OSAMU", "CHIKA", "HYUSE", "MIURA", "SOMEI", "OSANO", "HIURA", "AKANE", "SHIKI", "TOMOE", "AYUMU", "EBINA", "CHANO", "SAITO", "NANAO", "RYOGO", "MARUI", "SEIJI", "ASAMI", "HANAO", "ASUKA", "KEIZO", "IZUHO", "HINOE", "TRION", "ILGAR", "NABIS", "SCARE", "RADAR", "RIFLE", "COBRA", "EGRET", "SENKU", "GEIST", "TIMER", "TOKEN"];
 
 const wordOfTheDay = words[Math.floor(generator() * words.length)];
-
-console.log(wordOfTheDay);
